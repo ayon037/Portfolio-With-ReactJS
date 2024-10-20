96695e1ccafca8fdde7cd9b5d3323e153d6f74d7
ayon037 <mohiuddin.310799@gmail.com>
2024-10-20 13:44:59 +0600

    Update base path in vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Your repo name
+  base: '/Portfolio-With-ReactJS/',
+ // Your repo name
 });
