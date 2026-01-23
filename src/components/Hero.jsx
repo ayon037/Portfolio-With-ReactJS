00831886e78496f3fa4e2c1d59255acfa32a06f9
ayon037 <mohiuddin.310799@gmail.com>
2026-01-24 00:26:55 +0600

    Portfolio Updated for Edge Devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div className='z-10'>
+        <div className='z-10 relative'>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Mohiuddin Bilwal</span>
           </h1>
